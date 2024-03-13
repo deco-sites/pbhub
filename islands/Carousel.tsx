5390f814e42f41afb99b9a50b5d40f7f1ff85d86
Gustavo Lima <gustavowell098@gmail.com>
2024-03-13 19:57:52 -0300

    feat: created carousel

---
+++ islands/Carousel.tsx
@@ -0,0 +1,5 @@
+import Component, { Props } from "$store/components/carousel/Carousel.tsx";
+
+export default function Island(props: Props) {
+  return <Component {...props} />;
+}
--- sections/Content/Depositions.tsx
+++ sections/Content/Depositions.tsx
@@ -1,15 +1,1 @@
-import SliderJS from "$store/components/ui/SliderJS.tsx";
-
-export default function Depositions() {
-  return (
-    <div class="w-full bg-[#e6e8e6]">
-      <div class="flex lg:container lg:max-w-7xl lg:mx-auto mx-5 md:mx-10 justify-evenly">
-        {
-          /* <SliderJS>
-          <div class="w-">
-        </SliderJS> */
-        }
-      </div>
-    </div>
-  );
-}
+export { default } from "$store/islands/Carousel.tsx";
