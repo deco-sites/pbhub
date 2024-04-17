51a56c40a8587269f2f2d7183dbe2551a799754b
JeshuaBen <dev.09@pbhub.com.br>
2024-04-17 11:59:47 -0300

    feat: adding section pbhubMainCarousel

---
+++ islands/PbhubCarousel.tsx
@@ -0,0 +1,8 @@
+import Component from "$store/components/pbhub-carousel/Component.tsx";
+import type { Props } from "$store/components/pbhub-carousel/Component.tsx";
+
+function Island(props: Props) {
+  return <Component {...props} />;
+}
+
+export default Island;
